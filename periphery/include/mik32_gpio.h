6f2c51161f574224b7cbc824145e92ed11cbc5ae
Sergey Tkachenko <sa.tkachenko@mettem-m.ru>
2022-11-08 18:25:54 +0300

    Naming changes

--- periphery/include/mik32_gpio.h
+++ periphery/include/mik32_gpio.h
@@ -1,30 +1,45 @@
 #ifndef GPIO_H_INCLUDED
 #define GPIO_H_INCLUDED
 
-#define GPIO_STATE				0x00 
-#define GPIO_SET				0x00 
-#define GPIO_CLEAR				0x04 
-#define GPIO_DIRECTION_OUT		0x08 
-#define GPIO_DIRECTION_IN		0x0C 
-#define GPIO_OUTPUT             0x10
-#define GPIO_CONTROL			0x14 
+#define GPIO_STATE 0x00
+#define GPIO_SET 0x00
+#define GPIO_CLEAR 0x04
+#define GPIO_DIRECTION_OUT 0x08
+#define GPIO_DIRECTION_IN 0x0C
+#define GPIO_OUTPUT 0x10
+#define GPIO_CONTROL 0x14
 
-
+#define GPIO_PIN_0 0
+#define GPIO_PIN_1 1
+#define GPIO_PIN_2 2
+#define GPIO_PIN_3 3
+#define GPIO_PIN_4 4
+#define GPIO_PIN_5 5
+#define GPIO_PIN_6 6
+#define GPIO_PIN_7 7
+#define GPIO_PIN_8 8
+#define GPIO_PIN_9 9
+#define GPIO_PIN_10 10
+#define GPIO_PIN_11 11
+#define GPIO_PIN_12 12
+#define GPIO_PIN_13 13
+#define GPIO_PIN_14 14
+#define GPIO_PIN_15 15
 
 #ifndef _ASSEMBLER_
-    #include <inttypes.h>
+#include <inttypes.h>
 
-    typedef struct
-    {
-        volatile uint32_t SET;                 
-        volatile uint32_t CLEAR;               
-        volatile uint32_t DIRECTION_OUT;       
-        volatile uint32_t DIRECTION_IN;        
-        volatile uint32_t OUTPUT;
-        volatile uint32_t CONTROL;  
-        
-    } GPIO_TypeDef;
+typedef struct
+{
+    volatile uint32_t SET;
+    volatile uint32_t CLEAR;
+    volatile uint32_t DIRECTION_OUT;
+    volatile uint32_t DIRECTION_IN;
+    volatile uint32_t OUTPUT;
+    volatile uint32_t CONTROL;
+
+} GPIO_TypeDef;
 
 #endif
 
-#endif    
+#endif
--- periphery/include/mik32_pad_config.h
+++ periphery/include/mik32_pad_config.h
@@ -1,24 +1,38 @@
 #ifndef PAD_CONFIG_H_INCLUDED
 #define PAD_CONFIG_H_INCLUDED
 
+#define PAD_CFG_MODE_LEN 2
+#define PAD_CFG_MODE0 0x00
+#define PAD_CFG_MODE1 0x01
+#define PAD_CFG_MODE2 0x02
+#define PAD_CFG_MODE3 0x03
 
+#define PAD_DS_LEN 2
+#define PAD_DS_2MA 0x00
+#define PAD_DS_4MA 0x01
+#define PAD_DS_8MA 0x02
+#define PAD_DS_16MA 0x03 // TODO: check datasheet
 
+#define PAD_PUPD_LEN 2
+#define PAD_PUPD_OFF 0x00
+#define PAD_PUPD_UP 0x01
+#define PAD_PUPD_DOWN 0x02
 
 #ifndef _ASSEMBLER_
-    #include <inttypes.h>
+#include <inttypes.h>
 
-    typedef struct
-    {
-        volatile uint32_t PORT_0_CFG;                 
-        volatile uint32_t PORT_1_CFG;               
-        volatile uint32_t PORT_2_CFG;
-        volatile uint32_t PORT_0_DS;
-        volatile uint32_t PORT_1_DS;
-        volatile uint32_t PORT_2_DS;
-        volatile uint32_t PORT_0_PUD;
-        volatile uint32_t PORT_1_PUD;
-        volatile uint32_t PORT_2_PUD;
-    } PAD_CONFIG_TypeDef;
+typedef struct
+{
+    volatile uint32_t PORT0_CFG;
+    volatile uint32_t PORT1_CFG;
+    volatile uint32_t PORT2_CFG;
+    volatile uint32_t PORT0_DS;
+    volatile uint32_t PORT1_DS;
+    volatile uint32_t PORT2_DS;
+    volatile uint32_t PORT0_PUPD;
+    volatile uint32_t PORT1_PUPD;
+    volatile uint32_t PORT2_PUPD;
+} PAD_CONFIG_TypeDef;
 
 #endif
 
