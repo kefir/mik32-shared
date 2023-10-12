0ec9b43f0067a960971ead65db79cef82b859950
Sergey Tkachenko <sa.tkachenko@mettem-m.ru>
2023-10-12 18:06:12 +0300

    Fixed EPIC addresses

--- periphery/include/mik32_epic.h
+++ periphery/include/mik32_epic.h
@@ -2,31 +2,30 @@
 #define EPIC_H_INCLUDED
 
 
-#define EPIC_IRQ_MASK_SET           0x00
-#define EPIC_IRQ_MASK_CLEAR         0x04
-#define EPIC_IRQ_SW_SET             0x08
-#define EPIC_IRQ_SW_CLEAR           0x0C
-#define EPIC_IRQ_CLEAR              0x10
-#define EPIC_IRQ_STATUS             0x14
-#define EPIC_IRQ_RAW_STATUS         0x18
-
-
-
+#define EPIC_MASK_EDGE_SET          0x00
+#define EPIC_MASK_EDGE_CLEAR        0x04
+#define EPIC_MASK_LEVEL_SET         0x08
+#define EPIC_MASK_LEVEL_CLEAR       0x0C
+#define EPIC_SW_SET                 0x10
+#define EPIC_SW_CLEAR               0x14
+#define EPIC_CLEAR                  0x18
+#define EPIC_STATUS                 0x1C
+#define EPIC_RAW_STATUS             0x20
 
 #ifndef _ASSEMBLER_
     #include <inttypes.h>
 
     typedef struct
     {  
-        volatile uint32_t MASK_SET;
-        volatile uint32_t MASK_CLEAR;
-        volatile uint32_t MASK_LEVEL_SET;
-        volatile uint32_t MASK_LEVEL_CLEAR;        
-        volatile uint32_t SW_SET;
-        volatile uint32_t SW_CLEAR;
-        volatile uint32_t CLEAR;
-        volatile uint32_t STATUS;
-        volatile uint32_t RAW_STATUS;      
+        volatile uint32_t MASK_EDGE_SET;        // 0x00
+        volatile uint32_t MASK_EDGE_CLEAR;      // 0x04
+        volatile uint32_t MASK_LEVEL_SET;       // 0x08
+        volatile uint32_t MASK_LEVEL_CLEAR;     // 0x0C
+        volatile uint32_t SW_SET;               // 0x10
+        volatile uint32_t SW_CLEAR;             // 0x14
+        volatile uint32_t CLEAR;                // 0x18
+        volatile uint32_t STATUS;               // 0x1C
+        volatile uint32_t RAW_STATUS;           // 0x20
     } EPIC_TypeDef;
 #endif
 
